c3219ece18723e6eb2648981c82b2836c3fe2478
joaolucas14 <jlparceiro10@gmail.com>
2025-01-23 11:42:34 -0300

    Refatora o componente Cabecalho para incluir logotipo, título e links de navegação, melhorando a estrutura e o estilo do cabeçalho.

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,30 @@
 import logo from "../../assets/imagens/logo.png";
 import "./Cabecalho.css";
+import login from "./img/login.png";
+import cadastro from "./img/cadastro.png";
+import { Link } from "react-router-dom";
+
 export default function Cabecalho() {
   return (
     <header>
-      <img src={logo} alt="Logo" />
-      <p>Pokedex</p>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <h1>Pokedex</h1>
+      </div>
+      <div className="menu">
+        <Link to="#" className="link">
+          <div className="entrar">
+            <img src={login} alt="" />
+            <h2>Entrar</h2>
+          </div>
+        </Link>
+        <Link to="#" className="link">
+          <div className="cadastrar">
+            <img src={cadastro} alt="" />
+            <h2>Cadastre-se</h2>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
