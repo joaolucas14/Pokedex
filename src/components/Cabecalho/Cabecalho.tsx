ddd95e312ce17827ebd2e6066f92dd4cc854d5cd
joaolucas14 <jlparceiro10@gmail.com>
2025-02-03 16:50:54 -0300

    Adiciona componente ModalCadastro para gerenciar o cadastro de usuários e ajusta o cabeçalho para incluir a nova modal.

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,9 @@
 import logo from "../../assets/imagens/logo.png";
 import "./Cabecalho.css";
-import cadastro from "./img/cadastro.png";
 import favorito from "./img/favorito (2).png";
 import { Link } from "react-router-dom";
 import ModalLogin from "../ModalLogin/ModalLogin";
+import ModalCadastro from "../ModalCadastro/ModalCadastro";
 
 export default function Cabecalho() {
   return (
@@ -21,14 +21,8 @@ export default function Cabecalho() {
             <h2>Favoritos</h2>
           </div>
         </Link>
-        {/* Utilize o componente ModalLogin para abrir a modal de login */}
         <ModalLogin />
-        <Link to="#" className="link">
-          <div className="cadastrar">
-            <img src={cadastro} alt="" />
-            <h2>Cadastre-se</h2>
-          </div>
-        </Link>
+        <ModalCadastro />
       </div>
     </header>
   );
